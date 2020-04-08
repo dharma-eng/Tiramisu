c232bf3bddfb699aa21fc787e9e4c7da2160648b
dillon <dillonkellar@protonmail.com>
2020-04-08 14:29:57 -0500

    Updated withdrawal function.
    Added comments to describe the process. Added update for block hash.

--- contracts/DharmaPeg.sol
+++ contracts/DharmaPeg.sol
@@ -55,6 +55,10 @@ contract DharmaPeg is Owned, StateManager {
   /**
    * @dev executeWithdrawal
    * @notice Executes a withdrawal which exists in a confirmed block and replaces the leaf with a null value.
+   * @param header Block header which contains the transaction.
+   * @param transaction Encoded hard or soft withdrawal transaction.
+   * @param transactionIndex Location of the transaction in the transactions tree for the block.
+   * @param inclusionProof Array of sibling hashes for the transaction in the merkle tree.
    */
   function executeWithdrawal(
     Block.BlockHeader memory header,
@@ -62,19 +66,10 @@ contract DharmaPeg is Owned, StateManager {
     uint256 transactionIndex,
     bytes32[] memory inclusionProof
   ) external {
-    bytes32 blockHash = Block.blockHash(header);
-    require(blockIsConfirmed(header.blockNumber, blockHash), "Block is not confirmed.");
-    (bool included, bytes32 newRoot) = Merkle.verifyAndUpdate(
-      header.transactionsRoot,
-      transaction,
-      bytes(""),
-      transactionIndex,
-      inclusionProof
-    );
-    require(included, "Invalid inclusion proof.");
     byte txPrefix = transaction[0];
     uint56 value;
     address receiver;
+    /* Make sure the transaction is of the correct type and get the withdrawal parameters. */
     if (txPrefix == 0x02) {
       TX.HardWithdrawal memory withdrawal = TX.decodeHardWithdrawal(transaction);
       value = withdrawal.value;
@@ -84,7 +79,24 @@ contract DharmaPeg is Owned, StateManager {
       value = withdrawal.value;
       receiver = withdrawal.withdrawalAddress;
     } else revert("Transaction not of a withdrawal type.");
-    /* Add decimal conversion */
+    /* Verify that the block is confirmed. */
+    bytes32 blockHash = Block.blockHash(header);
+    require(blockIsConfirmed(header.blockNumber, blockHash), "Block is not confirmed.");
+    /* Verify the inclusion proof for the transaction and replace it with a null leaf. */
+    (bool included, bytes32 newRoot) = Merkle.verifyAndUpdate(
+      header.transactionsRoot,
+      transaction,
+      bytes(""),
+      transactionIndex,
+      inclusionProof
+    );
+    require(included, "Invalid inclusion proof.");
+    /* Update header with new transactions root. */
+    header.transactionsRoot = newRoot;
+    /* Update block hash with new header. */
+    blockHashes[header.blockNumber] = Block.blockHash(header);
+    /* Transfer DAI to the recipient. */
+    /* TODO - Add decimal conversion */
     tokenContract.transfer(receiver, value);
   }
 
