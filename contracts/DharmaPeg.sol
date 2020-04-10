7f5feed3f7d304465b7994b75c9a0c9568408ba3
dillon <dillonkellar@protonmail.com>
2020-04-10 15:49:44 -0500

    fix(contracts): DharmaPeg.sol
    Changed uint256 dai values to uint56 for consistency.
    Fixed some event use by moving it from the library.
    Added a constructor to initialize the config.
    Fixed the contract address source in the deposit function.
    Changed executeWithdrawal from external to public, to please the solc gods.
    Fixed a struct reference to reference the correct library.
    Fixed reference to dai contract.
    Fixed references to renamed internal functions.

--- contracts/DharmaPeg.sol
+++ contracts/DharmaPeg.sol
@@ -10,45 +10,69 @@ import "./lib/Owned.sol";
 import "./StateManager.sol";
 
 contract DharmaPeg is Owned, StateManager {
-  bytes[] public hardTransactions;
+  bytes[] internal hardTransactions;
 
-  function _deposit(address contractAddress, address signerAddress, uint256 value) {
-    uint32 value;
-    assembly {
-      let mask := 0x00000000000000000000000000000000000000000000000000ffffffffffffff
-      value := and(mask, _value)
+  constructor(
+    uint256 _challengePeriod,
+    uint256 _commitmentBond,
+    uint256 _version,
+    uint256 _changeDelay,
+    DharmaAddressHandler _addressHandler,
+    IERC20 _daiContract
+  ) public {
+    challengePeriod = _challengePeriod;
+    commitmentBond = _commitmentBond;
+    version = _version;
+    changeDelay = _changeDelay;
+    addressHandler = _addressHandler;
+    daiContract = _daiContract;
+  }
+
+  function getDepositsFrom(uint256 start, uint256 max)
+  external view returns (HardTx.HardDeposit[] memory deposits) {
+    uint256 stopAt = start + max;
+    uint256 len = hardTransactions.length;
+    if (stopAt > len) stopAt = len;
+    deposits = new HardTx.HardDeposit[](stopAt - start);
+    for (uint256 i = start; i < stopAt; i++) {
+      deposits[i] = HardTx.decodeHardDeposit(hardTransactions[i]);
     }
+  }
+
+  event NewHardTransaction(uint256 hardTransactionIndex/* , bytes hardTransaction */);
+
+  function _deposit(address contractAddress, address signerAddress, uint56 value) internal {
     /* Note - need to add unit conversion for correct decimal values */
-    require(daiContract.transferFrom(contractAddress, address(this), value), "Transfer Failed.");
+    require(daiContract.transferFrom(contractAddress, address(this), uint256(value)), "Transfer Failed.");
     /* TODO - replace storage of full data with storage of a hash, and emit the data in the event */
     HardTx.HardDeposit memory deposit = HardTx.HardDeposit(msg.sender, signerAddress, value);
-    emit HardTx.NewHardTransaction(hardTransactions.length);
+    emit NewHardTransaction(hardTransactions.length);
     hardTransactions.push(abi.encode(deposit));
   }
 
-  function deposit(uint256 value) external {
+  function deposit(uint56 value) external {
     address contractAddress = addressHandler.getContractAddressForSigner(msg.sender);
     _deposit(contractAddress, msg.sender, value);
   }
 
-  function deposit(address signerAddress, uint256 _value) external {
+  function deposit(address signerAddress, uint56 value) external {
     /* Need to figure out better logic for address mapping. */
     require(
       addressHandler.verifySignerHasAuthority(msg.sender, signerAddress),
       "Signer address does not match caller."
     );
-    _deposit(contractAddress, signerAddress, value);
+    _deposit(msg.sender, signerAddress, value);
   }
 
   function forceAddSigner(uint32 accountIndex, address signingAddress) external {
     HardTx.HardAddSigner memory hardTx = HardTx.HardAddSigner(accountIndex, msg.sender, signingAddress);
-    emit HardTx.NewHardTransaction(hardTransactions.length);
+    emit NewHardTransaction(hardTransactions.length);
     hardTransactions.push(abi.encode(hardTx));
   }
 
   function forceWithdrawal(uint32 accountIndex, uint56 value) external {
     HardTx.HardWithdrawal memory hardTx = HardTx.HardWithdrawal(accountIndex, msg.sender, value);
-    emit HardTx.NewHardTransaction(hardTransactions.length);
+    emit NewHardTransaction(hardTransactions.length);
     hardTransactions.push(abi.encode(hardTx));
   }
   
@@ -65,7 +89,7 @@ contract DharmaPeg is Owned, StateManager {
     bytes memory transaction,
     uint256 transactionIndex,
     bytes32[] memory inclusionProof
-  ) external {
+  ) public {
     byte txPrefix = transaction[0];
     uint56 value;
     address receiver;
@@ -75,7 +99,7 @@ contract DharmaPeg is Owned, StateManager {
       value = withdrawal.value;
       receiver = withdrawal.withdrawalAddress;
     } else if (txPrefix == 0x04) {
-      SoftWithdrawal memory withdrawal = TX.decodeSoftWithdrawal(transaction);
+      TX.SoftWithdrawal memory withdrawal = TX.decodeSoftWithdrawal(transaction);
       value = withdrawal.value;
       receiver = withdrawal.withdrawalAddress;
     } else revert("Transaction not of a withdrawal type.");
@@ -97,14 +121,14 @@ contract DharmaPeg is Owned, StateManager {
     blockHashes[header.blockNumber] = Block.blockHash(header);
     /* Transfer DAI to the recipient. */
     /* TODO - Add decimal conversion */
-    tokenContract.transfer(receiver, value);
+    daiContract.transfer(receiver, value);
   }
 
-  function submitBlock(Block.BlockInput calldata input) external onlyOwner {
-    putPendingBlock(input);
+  function submitBlock(Block.BlockInput memory input) public onlyOwner {
+    _putPendingBlock(input);
   }
 
-  function confirmBlock(Block.BlockHeader memory header) external {
-    confirmBlock(header);
+  function confirmBlock(Block.BlockHeader calldata header) external {
+    _confirmBlock(header);
   }
 }
