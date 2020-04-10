9d2984d45d6144b8028fcfef99501283fb8784b7
dillon <dillonkellar@protonmail.com>
2020-04-10 15:44:03 -0500

    update(contracts): MockDharmaPeg.sol - Added mock peg contract for easy deposit testing.
    Until we have a good address mapper in place, this will make it easy to deploy a peg chain and start submitting deposits.

---
+++ contracts/mocks/MockDharmaPeg.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+import "../DharmaPeg.sol";
+import "./DaiMock.sol";
+import "../interfaces/IERC20.sol";
+import "../interfaces/IDharmaAddressGetter.sol";
+
+abstract contract MockDharmaPeg is DharmaPeg {
+  constructor() public DharmaPeg(
+    50, /* challenge period */
+    50, /* commitment bond */
+    0, /* version */
+    0, /* config change delay */
+    IDharmaAddressGetter(address(0)), /* dharma addressHandler */
+    IERC20(address(new DaiMock(
+      5000, /* total supply */
+      string("DharmaDAI"), /* token name */
+      string("DDAI")
+    )))
+  ) {}
+
+  function mockDeposit(address contractAddress, address initialSignerAddress, uint56 value) external {
+    _deposit(contractAddress, initialSignerAddress, value);
+  }
+}
