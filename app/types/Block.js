c10465f38f9e676cc81e45aac748b096c7762390
dillon <dillonkellar@protonmail.com>
2020-04-11 02:18:03 -0500

    update(app): Block class - general refactor of approach, added full logic for block handling
    Cleaned up the block class and added handling for derivation of header fields. The block class now takes transactions and handles the derivation of the transactions root, transaction metadata and transactionsData fields in the block. Added functions to give commitment data once the block is submitted.

--- app/types/Block.js
+++ app/types/Block.js
@@ -1,41 +1,90 @@
 
 const { getMerkleRoot } = require('../../utils/merkle');
 const { toInt, toBuf, toHex } = require('../lib/to');
+const TransactionsMetadata = require('./TransactionMetadata');
+const { keccak256 } = require('ethereumjs-utils');
+
+const keys = [
+  'hardCreates',
+  'hardDeposits',
+  'hardWithdrawals',
+  'hardAddSigners',
+  'softWithdrawals',
+  'softCreates',
+  'softTransfers',
+  'softChangeSigners'
+]
 
 class Block {
   constructor({
-    transactionCounts,
-    transactionBuffers,
-    transactionLeaves,
+    version,
     blockNumber,
     stateSize,
+    stateRoot,
     hardTransactionsIndex,
-    stateRoot
+    transactions,
   }) {
-    const hardTransactionsCount = hardTransactionsIndex + transactionCounts.slice(0, 4).reduce((a, b) => a+b, 0);
-    const transactionsMeta = transactionCounts.map(count => toBuf(count, 2));
+    const transactionsArray = keys.reduce(
+      (arr, key) => [...arr, ...transactions[key]],
+      []
+    );
+
+    /* Encode transactions with their prefixes, calculate merkle root. */
+    const leaves = transactionsArray.map(t => t.encode(true));
+    const transactionsRoot = getMerkleRoot(leaves);
+
+    /* Encode transactions without their prefixes and concatenate them. Place the encoded metadata at the beginning. */
+    const transactionsMetadata = TransactionsMetadata.fromTransactions(transactions);
+    const transactionsBuffer = Buffer.concat(transactionsArray.map(t => t.encode(false)));
     const transactionsData = Buffer.concat([
-      Buffer.concat(transactionsMeta),
-      Buffer.concat(transactionBuffers)
+      transactionsMetadata.encode(),
+      transactionsBuffer
     ]);
-    const transactionsRoot = getMerkleRoot(transactionLeaves);
-    return {
-      header: {
-        version,
-        blockNumber,
-        stateSize,
-        hardTransactionsCount,
-        stateRoot,
-        transactionsRoot
-      },
-      transactionsData
+    
+    /* Add the hard transactions count from this block to the previous total. */
+    const hardTransactionsCount = hardTransactionsIndex + transactionsMetadata.hardTransactionsCount;
+    this.header = {
+      version,
+      blockNumber,
+      stateSize,
+      hardTransactionsCount: hardTransactionsCount,
+      stateRoot,
+      transactionsRoot
+    };
+    this.transactionsData = transactionsData;
+  }
+
+  addOutput(submittedAt) {
+    this.commitment = {
+      ...this.header,
+      transactionsHash: toHex(keccak256(this.transactionsData)),
+      submittedAt
     }
+  }
+
+  /* Currently just using ABI for this. */
+  blockHash(web3) {
+    if (!this.commitment) {
+      throw new Error('Blockhash not available! Requires calling `addOutput` with the block number from submission to L1.');
+    }
+    const structDef = {
+      BlockHeader: {
+        version: 'uint16',
+        blockNumber: 'uint32',
+        stateSize: 'uint32',
+        stateRoot: 'bytes32',
+        hardTransactionsCount: 'uint40',
+        transactionsRoot: 'bytes32',
+        transactionsHash: 'bytes32',
+        submittedAt: 'uint256',
+      }
+    };
+    const data = toBuf(web3.eth.abi.encodeParameter(
+      structDef,
+      this.commitment
+    ));
+    return toHex(keccak256(data));
   }
 }
 
-uint16 version;
-    uint32 blockNumber;
-    uint32 stateSize;
-    uint40 hardTransactionCount;
-    bytes32 stateRoot;
-    bytes32 transactionsRoot;
+module.exports = Block;
