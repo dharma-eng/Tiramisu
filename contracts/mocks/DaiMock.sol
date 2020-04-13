6874a8b92c54b5470fbc1d3bf52c261d696a9df0
0age <0age@protonmail.com>
2020-04-13 13:28:39 -0400

    fix tests for 0.6.6

--- contracts/mocks/DaiMock.sol
+++ contracts/mocks/DaiMock.sol
@@ -227,7 +227,7 @@ library Address {
         require(address(this).balance >= amount, "Address: insufficient balance");
 
         // solhint-disable-next-line avoid-low-level-calls, avoid-call-value
-        (bool success, ) = recipient.call.value(amount)("");
+        (bool success, ) = recipient.call{value: amount}("");
         require(success, "Address: unable to send value, recipient may have reverted");
     }
 }
