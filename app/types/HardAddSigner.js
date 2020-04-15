aef15881d0400b8ba92ba5ef1beeee2c642616c1
dillon <dillonkellar@protonmail.com>
2020-04-15 06:58:39 -0500

    update(app): HardAddSigner - added HardAddSigner transaction type

---
+++ app/types/HardAddSigner.js
@@ -0,0 +1,44 @@
+const { toBuf, toHex, toInt } = require('../lib/to');
+
+class HardAddSigner {
+  get prefix() {
+    return 4;
+  }
+
+  constructor({
+    accountIndex,
+    hardTransactionIndex,
+    signingAddress,
+  }) {
+    this.accountIndex = toHex(accountIndex);
+    this.hardTransactionIndex = toInt(hardTransactionIndex);
+    this.signingAddress = toHex(signingAddress);
+    // this.contractAddress = toHex(contractAddress);
+    // this.signerAddress = toHex(signerAddress);
+  }
+
+  addOutput(intermediateStateRoot) {
+    this.intermediateStateRoot = toHex(intermediateStateRoot);
+  }
+
+  encode(prefix = false) {
+    const txIndex = toBuf(this.hardTransactionIndex, 5);
+    const acctIndex = toBuf(this.accountIndex, 4);
+    const signingAddress = toBuf(this.signingAddress, 20);
+    const root = toBuf(this.intermediateStateRoot, 32);
+    return Buffer.concat([
+      prefix ? toBuf(this.prefix, 1) : Buffer.alloc(0),
+      txIndex,
+      acctIndex,
+      signingAddress,
+      root
+    ]);
+  }
+
+  checkValid(account) {
+    if (account.hasSigner(this.signingAddress)) return `Invalid signing address. Account already has signer ${this.signingAddress}`;
+    if (account.signers.length == 10) return `Account signer array full.`;
+  }
+}
+
+module.exports = HardAddSigner;
