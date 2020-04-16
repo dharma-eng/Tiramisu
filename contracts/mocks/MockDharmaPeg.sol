7abc041f88f0c356471ec1a79220850b0bec96b4
dillon <dillonkellar@protonmail.com>
2020-04-15 23:34:28 -0500

    update(contracts): MockDharmaPeg - moved dai address to constructor args
    facilitates some of the js functionality

--- contracts/mocks/MockDharmaPeg.sol
+++ contracts/mocks/MockDharmaPeg.sol
@@ -2,22 +2,18 @@ pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
 import "../DharmaPeg.sol";
-import "./DaiMock.sol";
 import "../interfaces/IERC20.sol";
 import "../interfaces/IDharmaAddressGetter.sol";
 
 contract MockDharmaPeg is DharmaPeg {
-  constructor() public DharmaPeg(
+  constructor(address daiContract) public DharmaPeg(
     0, /* challenge period */
     50, /* commitment bond */
     0, /* version */
     0, /* config change delay */
     IDharmaAddressGetter(address(0)), /* dharma addressHandler */
-    IERC20(address(new DaiMock(
-      5000, /* total supply */
-      string("DharmaDAI"), /* token name */
-      string("DDAI")
-    )))
+    IERC20(daiContract)
+
   ) {}
 
   function mockDeposit(address contractAddress, address initialSignerAddress, uint56 value) external {
