fde23b430e022dcfa3c82e97ab2d15f8f5a098c4
dillon <dillonkellar@protonmail.com>
2020-04-15 23:53:04 -0500

    update(app): Blockchain - hard tx decoding, constructor
    Added better constructor argument passing. Modified  to use the new hard transaction decoder.

--- app/Blockchain.js
+++ app/Blockchain.js
@@ -1,19 +1,20 @@
-const { deploy } = require('../utils/test-utils/web3');
-const { getMerkleRoot } = require('../utils/merkle');
-const { compileBaseMock } = require('./lib/compile');
-const sortTransactions = require('./lib/transaction-sort');
-const { Block, HardCreate, HardDeposit } = require('./types')
-const State = require('./state/State');
-const StateMachine = require('./state/StateMachine');
-const contracts = compileBaseMock();
+// const { deploy } = require('../utils/test-utils/web3');
+const {
+  decodeHardTransactions,
+  sortTransactions,
+  compileBaseMock
+} = require("./lib");
+const { Block } = require("./types");
+const StateMachine = require("./state/StateMachine");
 
 class Blockchain {
-  constructor(web3, fromAddress, peg, state) {
+  constructor({ web3, fromAddress, dai, peg, state }) {
     this.queue = [];
     this.hardTransactionsIndex = 0;
     this.maxHardTransactions = 10;
     this.address = fromAddress;
     this.web3 = web3;
+    this.dai = dai;
     this.peg = peg;
     this.state = state;
     this.stateMachine = new StateMachine(state);
@@ -21,37 +22,19 @@ class Blockchain {
     this.blockNumber = 0;
   }
 
-  static async create(web3, fromAddress) {
-    const peg = await deploy(web3, fromAddress, {
-      contracts,
-      name: 'MockDharmaPeg',
-      arguments: []
-    });
-    const state = await State.create();
-    return new Blockchain(web3, fromAddress, peg, state);
-  }
-
   async getHardTransactions() {
     /* only deposits and creates supported for now */
-    const deposits = await this.peg.methods.getDepositsFrom(this.hardTransactionsIndex, this.maxHardTransactions).call();
-    const arr = [];
-    for (let i = 0; i < deposits.length; i++) {
-      const hardTransactionIndex = this.hardTransactionsIndex + i;
-      const deposit = deposits[i];
-      const accountIndex = await this.state.getAccountIndexByAddress(deposit.contractAddress)
-      if (accountIndex != null) { 
-        arr.push(new HardDeposit({
-          hardTransactionIndex,
-          accountIndex,
-          ...deposit
-        }));
-      } else {
-        arr.push(new HardCreate({
-          hardTransactionIndex,
-          ...deposit
-        }))
-      }
-    }
+    const hardTransactions = await this.peg.methods
+      .getHardTransactionsFrom(
+        this.hardTransactionsIndex,
+        this.maxHardTransactions
+      )
+      .call();
+    const arr = await decodeHardTransactions(
+      this.state,
+      this.hardTransactionsIndex,
+      hardTransactions
+    );
     return arr;
   }
 
@@ -62,7 +45,10 @@ class Blockchain {
       softTransactions = [...this.queue];
       this.queue = [];
     } else softTransactions = [];
-    const transactions = sortTransactions([...hardTransactions, ...softTransactions]);
+    const transactions = sortTransactions([
+      ...hardTransactions,
+      ...softTransactions
+    ]);
     return transactions;
   }
 
@@ -70,16 +56,16 @@ class Blockchain {
     return new Promise(async (resolve, reject) => {
       transaction.assignResolvers(resolve, reject);
       this.queue.push(transaction);
-    })
+    });
   }
 
   async processBlock() {
-    const { hardTransactionsIndex, version, } = this;
+    const { hardTransactionsIndex, version } = this;
     const transactions = await this.getTransactions();
     await this.stateMachine.execute(transactions);
     const stateSize = this.state.size;
     const stateRoot = await this.state.rootHash();
-    
+
     const block = new Block({
       version,
       blockNumber: this.blockNumber,
@@ -95,21 +81,22 @@ class Blockchain {
   }
 
   async submitBlock(block) {
-    const receipt = await this.peg.methods.submitBlock(block).send({ gas: 5e6, from: this.address });
-    const { events: { BlockSubmitted: { blockNumber } } } = receipt;
+    const receipt = await this.peg.methods
+      .submitBlock(block)
+      .send({ gas: 5e6, from: this.address });
+    const {
+      events: {
+        BlockSubmitted: { blockNumber }
+      }
+    } = receipt;
     block.addOutput(blockNumber);
   }
 
   async confirmBlock(block) {
     const header = block.commitment;
-    await this.peg.methods.confirmBlock(header).send({ gas: 5e6, from: this.address });
-  }
-
-  async getDaiContract() {
-    const address = await this.peg.methods.daiContract().call();
-    const { abi } = contracts["interfaces/IERC20.sol"]["IERC20"];
-    const daiContract = new this.web3.eth.Contract(abi, address);
-    return daiContract;
+    await this.peg.methods
+      .confirmBlock(header)
+      .send({ gas: 5e6, from: this.address });
   }
 }
 
