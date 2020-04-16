b7826ccbe12de2dcd9bff48ef61660c2ba09464a
Anna Carroll <anna.s.carroll@gmail.com>
2020-04-16 14:45:05 -0700

    Create TransactionInterface for each transaction

--- app/types/TransactionInterfaces.ts
+++ app/types/TransactionInterfaces.ts
@@ -1,20 +1,69 @@
 export interface Transaction {
     prefix: number;
-    value: number,
-    intermediateStateRoot?: string,
+    intermediateStateRoot?: string;
+    accountIndex: number;
 }
+
+export interface CreateTransaction {
+    accountAddress: string;
+    initialSigningKey: string;
+}
+
+/**
+ * Hard Transactions -
+ *      Hard Create
+ *      Hard Deposit
+ *      Hard Withdraw
+ *      Hard Add Signer
+ */
 
 export interface HardTransaction extends Transaction {
-    hardTransactionIndex: number,
+    hardTransactionIndex: number;
 }
+
+export interface HardDepositTransaction extends HardTransaction {
+    value: number;
+}
+
+export interface HardCreateTransaction extends HardDepositTransaction, CreateTransaction {
+}
+
+export interface HardWithdrawTransaction extends HardTransaction {
+    value: number;
+}
+
+export interface HardAddSignerTransaction extends HardTransaction {
+    signingAddress: string;
+}
+
+
+/**
+ * Soft Transactions -
+ *      Soft Create
+ *      Soft Transfer
+ *      Soft Withdraw
+ *      Soft Change Signer
+ */
 
 export interface SoftTransaction extends Transaction {
-    nonce: number,
-    signature: string,
+    nonce: number;
+    signature: string;
 }
 
-export interface CreateTransaction extends Transaction{
-    contractAddress: string,
-    signerAddress: string,
-    accountIndex: number,
+export interface SoftTransferTransaction extends SoftTransaction {
+    toAccountIndex: number;
+    value: number;
+}
+
+export interface SoftCreateTransaction extends SoftTransferTransaction, CreateTransaction {
+}
+
+export interface SoftWithdrawTransaction extends SoftTransaction {
+    withdrawalAddress: string;
+    value: number;
+}
+
+export interface SoftChangeSignerTransaction extends SoftTransaction {
+    signingAddress: string;
+    modificationCategory: number;
 }
