07277fa3f3bd329d2830c6b9bb51f356f5aa6264
dillon <dillonkellar@protonmail.com>
2020-04-17 14:14:56 -0500

    fix(tests): BlockchainCoverage - fixed test that calls blockchain test

--- test/tests/BlockchainCoverage.spec.js
+++ test/tests/BlockchainCoverage.spec.js
@@ -1,15 +1,6 @@
-const MockDharmaDai = artifacts.require("MockDharmaDai");
-const MockDharmaPeg = artifacts.require("MockDharmaPeg");
-
 contract("Coverage Tests", accounts => {
   it("should be able to run coverage suite", async () => {
-    const MockDharmaDaiInstance = await MockDharmaDai.deployed();
-    const MockDharmaPegInstance = await MockDharmaDai.deployed(
-      MockDharmaDaiInstance.address
-    );
-
-    require("./Blockchain.spec.js");
-
-    return [MockDharmaDaiInstance, MockDharmaPegInstance];
+    const blockchainTest = require("./Blockchain.spec.js");
+    blockchainTest();
   });
 });
