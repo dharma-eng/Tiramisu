ef0df019f27c47025b175969f54ea57d12f275f7
Anna Carroll <anna.s.carroll@gmail.com>
2020-04-30 11:09:09 -0700

    Move import below exports

--- app/index.ts
+++ app/index.ts
@@ -1,8 +1,8 @@
-import setupServer from "./setupServer";
 export * from './types';
 export * from './state';
 export * from './lib';
 export {default as Blockchain} from './Blockchain';
+import setupServer from "./setupServer";
 
 setupServer();
 
