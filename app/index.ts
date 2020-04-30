610fca8b754063630c26ad5ee3f16a03d3b9479f
Anna Carroll <anna.s.carroll@gmail.com>
2020-04-30 10:37:16 -0700

    Setup server

--- app/index.ts
+++ app/index.ts
@@ -1,4 +1,20 @@
+import express from "express";
+const PORT = 5000;
+
+const server = express();
+server.listen(PORT, error => {
+  if (error) {
+    console.error("ERROR - Unable to start server.");
+  } else {
+    console.log(
+      `INFO - Server started on http://localhost:${PORT}`
+    );
+  }
+});
+
+
 export * from './types';
 export * from './state';
 export * from './lib';
 export {default as Blockchain} from './Blockchain';
+
