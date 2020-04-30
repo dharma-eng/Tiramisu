523eccbd5c4278031e80ba6e4fb927c6523a63fe
Anna Carroll <anna.s.carroll@gmail.com>
2020-04-30 11:08:42 -0700

    setupServer in dedicated file

--- app/index.ts
+++ app/index.ts
@@ -1,35 +1,8 @@
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import schema from "./schema";
-
-const PORT = 5000;
-const GRAPHQL_ENDPOINT = "/";
-
-const server = express();
-
-const context = ({ req }) => ({
-  req,
-});
-
-const apolloServer = new ApolloServer({ schema, context });
-
-apolloServer.applyMiddleware({
-  app: server,
-  path: GRAPHQL_ENDPOINT,
-});
-
-server.listen(PORT, error => {
-  if (error) {
-    console.error("ERROR - Unable to start server.");
-  } else {
-    console.log(
-      `INFO - Server started on http://localhost:${PORT}`
-    );
-  }
-});
-
+import setupServer from "./setupServer";
 export * from './types';
 export * from './state';
 export * from './lib';
 export {default as Blockchain} from './Blockchain';
+
+setupServer();
 
---
+++ app/setupServer.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import schema from "./graphql/schema";
+
+const PORT = 5000;
+const GRAPHQL_ENDPOINT = "/";
+
+function setupServer() {
+  const server = express();
+
+  const context = ({ req }) => ({
+    req,
+  });
+
+  const apolloServer = new ApolloServer({ schema, context });
+
+  apolloServer.applyMiddleware({
+    app: server,
+    path: GRAPHQL_ENDPOINT,
+  });
+
+  server.listen(PORT, error => {
+    if (error) {
+      console.error("ERROR - Unable to start server.");
+    } else {
+      console.log(
+        `INFO - Server started on http://localhost:${PORT}`
+      );
+    }
+  });
+}
+
+export default setupServer;
+
