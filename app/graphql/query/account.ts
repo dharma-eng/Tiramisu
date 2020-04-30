fb2917d0e11b0d176223aa002b54470f8517a454
Anna Carroll <anna.s.carroll@gmail.com>
2020-04-30 15:19:42 -0700

    Add tentative way to setup state machine + getAccount query

--- app/graphql/query/account.ts
+++ app/graphql/query/account.ts
@@ -1,14 +1,24 @@
 import { AccountType } from "../type/accountType";
-import { Account } from "../../types";
+import { GraphQLString } from "graphql";
+import { stateMachine } from "../../index";
 
 export const getAccount = {
   type: AccountType,
-  resolve: () => {
-    return new Account({
-      address: "0x030585f58f92AD5c0D0Ce457B8F9172534c6e9D1",
-      nonce: 0,
-      balance: 121.5,
-      signers: ["0xa6189D24a8254E7396C600dB96CE82f4EbE56F40", "0x260e6D0d9aE65B8eC765E3F58335150Ccd4Ef032"]
-    });
+  args: {
+    address: {
+      name: "address",
+      type: GraphQLString,
+    },
+  },
+  resolve: async (parentValue, { address }, auth) => {
+    const machine = await stateMachine;
+
+    const index = await machine.state.getAccountIndexByAddress(address);
+
+    if (index || index === 0) {
+      return machine.state.getAccount(index);
+    }
   },
 };
+
+
--- app/index.ts
+++ app/index.ts
@@ -1,8 +1,18 @@
+import { State, StateMachine } from "./state";
+
 export * from './types';
 export * from './state';
 export * from './lib';
 export {default as Blockchain} from './Blockchain';
 import setupServer from "./setupServer";
 
-setupServer(); 
+setupServer();
+
+export let stateMachine = setupStateMachine();
+
+async function setupStateMachine() {
+  const state = await State.create();
+  return new StateMachine(state);
+}
+
 
