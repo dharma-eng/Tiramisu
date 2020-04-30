765bd19797349aed7b3b33c386fdae1295d52f32
Anna Carroll <anna.s.carroll@gmail.com>
2020-04-30 10:52:08 -0700

    Setup minimal GraphQL

--- app/index.ts
+++ app/index.ts
@@ -1,7 +1,64 @@
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
 const PORT = 5000;
+const GRAPHQL_ENDPOINT = "/";
+
+const test = {
+  type: GraphQLString,
+  resolve: () => "Hello world!",
+};
+
+const query = new GraphQLObjectType({
+  name: "query",
+  description: "API Queries [Read]",
+  fields: () => ({
+    test
+  }),
+});
+
+const testPrint = {
+  type: GraphQLString,
+  args: {
+    stringToPrint: {
+      name: "stringToPrint",
+      type: GraphQLString,
+    }
+  },
+  resolve: (parentValue, { stringToPrint }, auth) => {
+    console.log(`PRINTING STRING: ${stringToPrint}`);
+  }
+};
+
+const mutation = new GraphQLObjectType({
+  name: "mutations",
+  description: "API Mutations [Create, Update, Delete]",
+  fields: () => ({
+    testPrint
+  }),
+});
+
+const types = [];
+
+const schema = new GraphQLSchema({
+  types,
+  query,
+  mutation,
+});
 
 const server = express();
+
+const context = ({ req }) => ({
+  req,
+});
+
+const apolloServer = new ApolloServer({ schema, context });
+
+apolloServer.applyMiddleware({
+  app: server,
+  path: GRAPHQL_ENDPOINT,
+});
+
 server.listen(PORT, error => {
   if (error) {
     console.error("ERROR - Unable to start server.");
@@ -11,7 +68,6 @@ server.listen(PORT, error => {
     );
   }
 });
-
 
 export * from './types';
 export * from './state';
