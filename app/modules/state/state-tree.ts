daad73e4d4959356746da6183b8bd9b22066fec9
dillon <dillonkellar@protonmail.com>
2020-05-04 23:56:38 -0500

    update(app/modules): state-tree - added LevelSideways support to state tree, cleaned up unused code

--- app/modules/state/state-tree.ts
+++ app/modules/state/state-tree.ts
@@ -1,27 +1,11 @@
-// const path = require('path');
-// const fs = require('fs');
-const { BaseDB, SparseMerkleTreeImpl, keccak256 } = require('sparse-merkle-tree');
-const MemDown = require('memdown');
-
-// const dbRootPath = path.join(__dirname, 'db');
-// fs.mkdirSync(dbRootPath, { recursive: true });
-
-// const hashBuffer = Buffer.alloc(64);
-const hashFunction = keccak256;
-const bufferHashFunction = (buff) =>
-  Buffer.from(hashFunction(buff.toString('hex')), 'hex')
-// const zeroHash = bufferHashFunction(SparseMerkleTreeImpl.emptyBuffer)
+import { BaseDB, SparseMerkleTreeImpl, keccak256 } from 'sparse-merkle-tree';
+import { LevelSideways } from '../../lib/simple-level';
 
 const depth = 32;
 
-const memdown = new MemDown();
-const db = new BaseDB(memdown, 256);
-
-const getTree = () => SparseMerkleTreeImpl.create(
-  db,
-  undefined,
+export const getTree = (db: LevelSideways, rootHash?: Buffer) => SparseMerkleTreeImpl.create(
+  new BaseDB(db, 256),
+  rootHash,
   depth,
-  hashFunction
+  keccak256
 );
-
-export default getTree;
