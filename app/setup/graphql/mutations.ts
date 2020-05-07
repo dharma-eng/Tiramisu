1b22272537f81c7f0186a0b6e93ad7784db183f3
Anna Carroll <anna.s.carroll@gmail.com>
2020-05-07 11:41:17 -0700

    Add queries and mutations to schema

--- app/setup/graphql/mutations.ts
+++ app/setup/graphql/mutations.ts
@@ -1,11 +1,19 @@
 import { GraphQLObjectType } from "graphql";
 import * as account from "../../modules/account/mutation";
+import * as softChangeSigner from "../../modules/transactions/soft-change-signer/mutation";
+import * as softTransfer from "../../modules/transactions/soft-transfer/mutation";
+import * as softWithdrawal from "../../modules/transactions/soft-withdrawal/mutation";
+import * as softCreate from "../../modules/transactions/soft-create/mutation";
 
 const mutations = new GraphQLObjectType({
   name: "mutations",
   description: "API Mutations [Create, Update, Delete]",
   fields: () => ({
     ...account,
+    ...softChangeSigner,
+    ...softTransfer,
+    ...softWithdrawal,
+    ...softCreate,
   }),
 });
 
--- app/setup/graphql/queries.ts
+++ app/setup/graphql/queries.ts
@@ -1,11 +1,19 @@
 import { GraphQLObjectType } from "graphql";
 import * as account from "../../modules/account/query";
+import * as softChangeSigner from "../../modules/transactions/soft-change-signer/query";
+import * as softTransfer from "../../modules/transactions/soft-transfer/query";
+import * as softWithdrawal from "../../modules/transactions/soft-withdrawal/query";
+import * as softCreate from "../../modules/transactions/soft-create/query";
 
 const queries = new GraphQLObjectType({
   name: "query",
   description: "API Queries [Read]",
   fields: () => ({
     ...account,
+    ...softChangeSigner,
+    ...softTransfer,
+    ...softWithdrawal,
+    ...softCreate,
   }),
 });
 
