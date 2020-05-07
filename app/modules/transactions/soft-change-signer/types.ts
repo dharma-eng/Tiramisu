bc9efaf95ddf3d30046053c257e2aa6d7b667ce8
Anna Carroll <anna.s.carroll@gmail.com>
2020-05-07 14:41:56 -0700

    Implement soft change signer resolvers

--- app/modules/transactions/soft-change-signer/types.ts
+++ app/modules/transactions/soft-change-signer/types.ts
@@ -1,11 +1,18 @@
-import { GraphQLInputObjectType, GraphQLInt, GraphQLObjectType, GraphQLString } from "graphql";
+import {
+  GraphQLBoolean,
+  GraphQLError,
+  GraphQLInputObjectType,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLString
+} from "graphql";
 import SoftChangeSigner from "./index";
 import { AccountType } from "../../account/types";
 
 export const SoftChangeSignerType = new GraphQLObjectType({
   name: "softChangeSigner",
   description: "Soft Change Signer Type",
-  isTypeOf: account => account instanceof SoftChangeSigner,
   fields: () => ({
     account: {
       type: AccountType
@@ -19,11 +26,20 @@ export const SoftChangeSignerType = new GraphQLObjectType({
     nonce: {
       type: GraphQLInt
     },
+    messageHash: {
+      type: GraphQLString
+    },
     signature: {
       type: GraphQLString
     },
     intermediateStateRoot: {
       type: GraphQLString
+    },
+    errors: {
+      type: GraphQLList(GraphQLString)
+    },
+    success: {
+      type: GraphQLBoolean
     }
   }),
 });
