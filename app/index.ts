e7d03278e4a9667ed3c3d6fd736465f020e0044c
Anna Carroll <anna.s.carroll@gmail.com>
2020-05-07 14:37:28 -0700

    Export L2Core as singleton

--- app/index.ts
+++ app/index.ts
@@ -1,9 +1,16 @@
 import {setupServer, NODE_ENV} from "./setup";
+import DharmaL2Core from "./l2-core";
 
 if (NODE_ENV === "develop") {
   setupServer();
 }
 
+//TODO: properly setup L2 core object that can be used throughout the app
+export const L2CorePromise = DharmaL2Core.create({
+  peg: {},
+  from: "0x0",
+  web3: {}
+});
 export * from "./setup";
 export * from './modules';
 export * from './lib';
