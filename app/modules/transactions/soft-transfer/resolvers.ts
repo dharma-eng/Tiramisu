9356fb6f9a435b4dca7cdb683e19182e2536d84c
Anna Carroll <anna.s.carroll@gmail.com>
2020-05-08 14:05:51 -0700

    Implement softTransfer resolvers and types

--- app/modules/transactions/soft-transfer/resolvers.ts
+++ app/modules/transactions/soft-transfer/resolvers.ts
@@ -1,15 +1,117 @@
-export async function submitSoftTransferResolver(
-  parentValue,
-  { },
-  auth
-) {
-  //TODO implement
-}
+import { L2CorePromise, toHex, UNSIGNED } from "../../../index";
+import SoftTransfer from "./";
+import TransactionQueue from "../../transactions-queue";
 
 export async function getUnsignedSoftTransferResolver(
   parentValue,
-  { },
+  { accountAddress, toAccountAddress, value },
   auth
 ) {
-  //TODO implement
+  //TODO: update pulling in L2Core
+  const L2Core = await L2CorePromise;
+  const state = await L2Core.getLatestState();
+
+  const accountIndex = await state.getAccountIndexByAddress(accountAddress);
+  const toAccountIndex = await state.getAccountIndexByAddress(toAccountAddress);
+
+  const errors = validateInputs(accountIndex, toAccountIndex);
+
+  if (errors.length > 0) {
+    return {
+      errors
+    };
+  }
+
+  const account = await state.getAccount(accountIndex);
+  const toAccount = await state.getAccount(toAccountIndex);
+
+  const softTransfer = new SoftTransfer({
+    accountIndex,
+    toAccountIndex,
+    nonce: account.nonce,
+    value,
+    signature: UNSIGNED
+  });
+
+  const error = softTransfer.checkValid(account, false);
+
+  if (error) {
+    errors.push(error);
+  }
+
+  const messageHash = toHex(softTransfer.toMessageHash());
+
+  return {
+    account,
+    toAccount,
+    ...softTransfer,
+    messageHash,
+    errors
+  };
+}
+
+export async function submitSoftTransferResolver(
+  parentValue,
+  { accountAddress, toAccountAddress, value, nonce, signature },
+  auth
+) {
+  //TODO: update pulling in L2Core
+  const L2Core = await L2CorePromise;
+  const state = await L2Core.getLatestState();
+
+  const accountIndex = await state.getAccountIndexByAddress(accountAddress);
+  const toAccountIndex = await state.getAccountIndexByAddress(toAccountAddress);
+
+  const errors = validateInputs(accountIndex, toAccountIndex);
+
+  if (errors.length > 0) {
+    return {
+      errors
+    };
+  }
+
+  const account = await state.getAccount(accountIndex);
+  const toAccount = await state.getAccount(toAccountIndex);
+
+  const softTransfer = new SoftTransfer({
+    accountIndex,
+    toAccountIndex,
+    nonce,
+    value,
+    signature
+  });
+
+  const error = softTransfer.checkValid(account, true);
+
+  let success;
+  if (error) {
+    errors.push(error);
+    success = false;
+  } else {
+    //TODO: this will return once the transaction has been executed. should we await here?
+    await TransactionQueue.queueTransaction(softTransfer);
+    success = true;
+  }
+
+  return {
+    account,
+    toAccount,
+    ...softTransfer,
+    errors,
+    success
+  };
+}
+
+function validateInputs(accountIndex: number, toAccountIndex: number,) {
+  const errors = [];
+
+  if (accountIndex === null) {
+    errors.push("Account does not exist for address; cannot transfer funds from account");
+  }
+
+  if (toAccountIndex === null) {
+    errors.push("Account does not exist for address; cannot transfer funds to account; try Soft Create transaction");
+  }
+
+  return errors;
 }
--- app/modules/transactions/soft-transfer/types.ts
+++ app/modules/transactions/soft-transfer/types.ts
@@ -1,26 +1,79 @@
-import { GraphQLInputObjectType, GraphQLObjectType } from "graphql";
+import {
+  GraphQLBoolean,
+  GraphQLFloat,
+  GraphQLInputObjectType,
+  GraphQLInt, GraphQLList,
+  GraphQLObjectType,
+  GraphQLString
+} from "graphql";
 import SoftTransfer from "./index";
+import { AccountType } from "../../account/types";
 
 export const SoftTransferType = new GraphQLObjectType({
   name: "softTransfer",
   description: "Soft Transfer Type",
   isTypeOf: account => account instanceof SoftTransfer,
   fields: () => ({
-    //TODO: define fields
+    account: {
+      type: AccountType
+    },
+    toAccount: {
+      type: AccountType
+    },
+    nonce: {
+      type: GraphQLInt
+    },
+    value: {
+      type: GraphQLFloat
+    },
+    signature: {
+      type: GraphQLString
+    },
+    prefix: {
+      type: GraphQLInt
+    },
+    intermediateStateRoot: {
+      type: GraphQLString
+    },
+    messageHash: {
+      type: GraphQLString
+    },
+    errors: {
+      type: GraphQLList(GraphQLString)
+    },
+    success: {
+      type: GraphQLBoolean
+    }
   }),
 });
 
-export const SubmitSoftTransferInput = new GraphQLInputObjectType({
-  name: 'submitSoftTransferInput',
-  fields: () => ({
-    //TODO: define fields
-  })
-});
+export const SubmitSoftTransferInput = {
+  accountAddress: {
+    type: GraphQLString
+  },
+  toAccountAddress: {
+    type: GraphQLString
+  },
+  value: {
+    type: GraphQLFloat
+  }
+};
 
-export const GetUnsignedSoftTransferInput = new GraphQLInputObjectType({
-  name: 'getUnsignedSoftTransferInput',
-  fields: () => ({
-    //TODO: define fields
-  })
-});
+export const GetUnsignedSoftTransferInput = {
+  accountAddress: {
+    type: GraphQLString
+  },
+  toAccountAddress: {
+    type: GraphQLString
+  },
+  value: {
+    type: GraphQLFloat
+  },
+  nonce: {
+    type: GraphQLInt
+  },
+  signature: {
+    type: GraphQLString
+  }
+};
 
