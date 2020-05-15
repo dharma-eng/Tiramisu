0b2131550e85ac8774341a31c6a5801448056a43
dillon <dillonkellar@protonmail.com>
2020-05-14 23:56:37 -0500

    fix(DharmaL2Core): Fixed confirmationTimer callback function

--- app/l2-core/index.ts
+++ app/l2-core/index.ts
@@ -23,7 +23,7 @@ export class DharmaL2Core extends EventEmitter {
     private dbPath?: string
   ) {
     super();
-    this._confirmationTimer = setTimeout(this.confirmationLoop, 5000);
+    this._confirmationTimer = setTimeout(() => this.confirmationLoop, 5000);
   }
 
   static async create(web3: Web3Options, dbPath?: string): Promise<DharmaL2Core> {
@@ -49,7 +49,7 @@ export class DharmaL2Core extends EventEmitter {
     } catch(err) {
       console.error(err);
     } finally {
-      this._confirmationTimer = setTimeout(this.confirmationLoop, 5000);
+      this._confirmationTimer = setTimeout(() => this.confirmationLoop, 5000);
     }
   }
 
