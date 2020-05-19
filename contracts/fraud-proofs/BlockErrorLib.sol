4af6a949e66a019f50c51c0581b2d3a03228ba98
dillon <dillonkellar@protonmail.com>
2020-05-19 12:15:43 -0500

    resolved merge conflict

--- contracts/fraud-proofs/BlockErrorLib.sol
+++ contracts/fraud-proofs/BlockErrorLib.sol
@@ -7,7 +7,7 @@ import { MerkleTreeLib as Merkle } from "../lib/merkle/MerkleTreeLib.sol";
 import { FraudUtilsLib as utils } from "./FraudUtilsLib.sol";
 
 
-library HeaderFraudProofs {
+library BlockErrorLib {
   using Block for bytes;
   using Block for Block.BlockHeader;
   using State for State.State;
@@ -52,42 +52,44 @@ library HeaderFraudProofs {
     }
   }
 
-  function putLeaves(
-    bytes[] memory leaves,
-    bool identitySuccess,
-    uint256 leafIndex,
-    uint256 currentPointer,
-    uint8 typePrefix,
-    uint256 typeCount,
-    uint256 typeSize
-  ) internal view returns (
-    bool _identitySuccess, uint256 _leafIndex, uint256 _currentPointer
-  ) {
-    // Iterate over a set of memory pointers dictated by typeSize and typeCount.
-    uint256 finalPointer = currentPointer + (typeSize * typeCount);
+  /**
+   * @dev proveStateRootError
+   * Proves that the state size in a block header does not match the expected state size based on
+   * the creation transactions in the block.
+   * @param state storage struct representing the peg state
+   * @param badHeader block header with error
+   * @param transactionsData transactions buffer from the block
+   */
+  function proveStateRootError(
+    State.State storage state,
+    Block.BlockHeader memory badHeader,
+    bytes memory transactionsData
+  ) internal {
+    require(
+      state.blockIsPending(badHeader.blockNumber, badHeader.blockHash()),
+      "Block not pending."
+    );
 
-    for (uint256 i = currentPointer; i < finalPointer; i += typeSize) {
-      // Allocate a memory region for the transaction and set the prefix.
-      bytes memory transaction = new bytes(typeSize + 1);
-      transaction[0] = byte(typePrefix);
+    require(
+      badHeader.hasTransactionsData(transactionsData),
+      "Header does not match transactions data."
+    );
 
-      assembly {
-        // Get target location in memory, skipping the length and prefix.
-        let targetPointer := add(transaction, 33)
-
-        // Perform a "memcpy" using the identity precompile contract.
-        identitySuccess := staticcall(
-          gas(), 4, i, typeSize, targetPointer, typeSize
-        )
-      }
-
-      // Update the relevant leaf and increment the index.
-      leaves[leafIndex++] = transaction;
-    }
-
-    return (identitySuccess, leafIndex, currentPointer);
+    bytes32 root;
+    // get the last 32 bytes of transactionsData, which will be the state root of the last transaction
+    assembly { root := mload(add(transactionsData, mload(transactionsData))) }
+    require(root != badHeader.stateRoot, "Block had correct state root.");
+    state.revertBlock(badHeader);
   }
-
+  
+  /**
+   * @dev proveTransactionsRootError
+   * Proves that the transactions root in a block header does not
+   * match the result of merkleizing the transactions data.
+   * @param state storage struct representing the peg state
+   * @param badHeader block header with error
+   * @param transactionsData transactions buffer from the block
+   */
   function proveTransactionsRootError(
     State.State storage state,
     Block.BlockHeader memory badHeader,
@@ -108,6 +110,49 @@ library HeaderFraudProofs {
     }
   }
 
+  /**
+   * @dev proveTransactionsDataLengthError
+   * Proves that the length of the transactions data in a block is invalid.
+   * "invalid" means that it either did not contain the transaction metadata
+   * or that the length is not consistent with the length expected from the
+   * metadata.
+   * @param state storage struct representing the peg state
+   * @param badHeader block header with error
+   * @param transactionsData transactions buffer from the block
+   */
+  function proveTransactionsDataLengthError(
+    State.State storage state,
+    Block.BlockHeader memory badHeader,
+    bytes memory transactionsData
+  ) internal {
+    require(
+      state.blockIsPending(badHeader.blockNumber, badHeader.blockHash()),
+      "Block not pending."
+    );
+
+    require(
+      badHeader.hasTransactionsData(transactionsData),
+      "Header does not match transactions data."
+    );
+
+    if (transactionsData.length >= 16) {
+      Tx.TransactionsMetadata memory meta = Tx.decodeTransactionsMetadata(transactionsData);
+      uint256 expectedLength = Tx.expectedTransactionsLength(meta);
+      require(transactionsData.length != expectedLength + 16, "Transactions data had correct length.");
+    }
+    state.revertBlock(badHeader);
+  }
+
+  /**
+   * @dev proveHardTransactionsCountError
+   * Proves that the `hardTransactionsCount` in the block header is not equal to
+   * the total number of hard transactions in the metadata plus the previous block's
+   * hard transactions count.
+   * @param state storage struct representing the peg state
+   * @param previousHeader header of the previous block
+   * @param badHeader block header with error
+   * @param transactionsData transactions buffer from the block
+   */
   function proveHardTransactionsCountError(
     State.State storage state,
     Block.BlockHeader memory previousHeader,
@@ -142,6 +187,10 @@ library HeaderFraudProofs {
   /**
    * @dev proveHardTransactionsRangeError
    * Proves that a block has a missing or duplicate hard transaction index.
+   * @param state storage struct representing the peg state
+   * @param previousHeader header of the previous block
+   * @param badHeader block header with error
+   * @param transactionsData transactions buffer from the block
    */
   function proveHardTransactionsRangeError(
     State.State storage state,
