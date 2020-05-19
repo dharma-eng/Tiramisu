1a5c8038791090e935e727f49e0483f27263e9ef
dillon <dillonkellar@protonmail.com>
2020-05-19 00:13:15 -0500

    update(auditor): Added hard tx order error type

--- app/modules/auditor/types/block-errors.ts
+++ app/modules/auditor/types/block-errors.ts
@@ -8,13 +8,15 @@ export type ProofData_TransactionsRoot = ErrorProofData & With_Transactions;
 export type ProofData_HardTransactionsCount = ErrorProofData & With_Parent & With_Transactions;
 export type ProofData_TransactionsLength = ErrorProofData & With_Transactions;
 export type ProofData_HardTransactionsRange = ErrorProofData & With_Parent & With_Transactions;
+export type ProofData_HardTransactionsOrder = ErrorProofData & With_Transactions;
 
 export type StateSizeError = ErrorType<"state_size", ProofData_StateSize>;
 export type StateRootError = ErrorType<"state_root", ProofData_StateRoot>;
 export type TransactionsRootError = ErrorType<"transactions_root", ProofData_TransactionsRoot>;
 export type HardTransactionsCountError = ErrorType<"hard_transactions_count", ProofData_HardTransactionsCount>;
-export type TransactionsLengthError = ErrorType<"transactions_length", ProofData_TransactionsLength>;
 export type HardTransactionsRangeError = ErrorType<"hard_transactions_range", ProofData_HardTransactionsRange>;
+export type HardTransactionsOrderError = ErrorType<"hard_transactions_order", ProofData_HardTransactionsOrder>;
+export type TransactionsLengthError = ErrorType<"transactions_length", ProofData_TransactionsLength>;
 
-export type BlockError = StateSizeError | StateRootError | TransactionsRootError |
-  HardTransactionsCountError | HardTransactionsRangeError | TransactionsLengthError;
+export type BlockError = StateSizeError | StateRootError | TransactionsRootError | HardTransactionsCountError |
+  HardTransactionsRangeError | HardTransactionsOrderError | TransactionsLengthError;
