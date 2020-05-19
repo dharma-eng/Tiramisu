69b830701c99df3e2354006361581933379dbd87
dillon <dillonkellar@protonmail.com>
2020-05-19 00:12:54 -0500

    update(auditor): Added type defs for proof function inputs

---
+++ app/modules/auditor/types/functions.ts
@@ -0,0 +1,53 @@
+import { Commitment } from "../../block";
+import { BufferLike } from "../../../lib";
+
+export type TransactionErrorLibFunctionName = 'proveHardTransactionSourceError' | 'proveSignatureError';
+
+export type TransactionErrorLibFunctionInput = {
+  name: TransactionErrorLibFunctionName;
+  data: [
+    Commitment, // header
+    BufferLike, // transaction
+    number, // transactionIndex
+    BufferLike[], // siblings
+    BufferLike, // previousStateProof
+    BufferLike // stateProof
+  ]
+}
+
+export type BlockErrorLibFunctionName = 'proveStateSizeError' | 'proveStateRootError' | 'proveTransactionsRootError' |
+'proveTransactionsDataLengthError' | 'proveHardTransactionsCountError' | 'proveHardTransactionsRangeError';
+
+export type BlockErrorLibFunctionInput = {
+  name: BlockErrorLibFunctionName;
+  data: [
+    Commitment, // previousHeader
+    Commitment, // header
+    BufferLike // transactionsData
+  ] | [
+    Commitment, // header
+    BufferLike // transactionsData
+  ]
+}
+
+export type ExecutionErrorLibFunctionName = 'proveExecutionError' | 'proveCreateIndexError';
+
+export type ExecutionErrorLibFunctionInput = {
+  name: ExecutionErrorLibFunctionName;
+  data: [
+    Commitment, // previousHeader
+    Commitment, // header
+    number, // transactionsIndex
+    BufferLike // transactionsData
+  ] | [
+    Commitment, // header
+    BufferLike, // transactionStateProof
+    BufferLike, // transaction
+    BufferLike, // stateProof1
+    BufferLike // stateProof2
+  ]
+}
+
+export type ErrorProofFunctionInput = TransactionErrorLibFunctionInput |
+  BlockErrorLibFunctionInput |
+  ExecutionErrorLibFunctionInput;
