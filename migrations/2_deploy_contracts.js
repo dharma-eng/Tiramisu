24ad57c208842607cd1d12097a45c238e558b579
dillon <dillonkellar@protonmail.com>
2020-05-28 12:19:16 -0500

    removed outdated link to library with no public fns

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,7 @@
 const MockTiramisu = artifacts.require("MockTiramisu");
 const MockToken = artifacts.require("MockToken");
-const MerkleProofLib = artifacts.require("MerkleProofLib");
 
 module.exports = function(deployer) {
-  deployer.deploy(MerkleProofLib);
-  deployer.link(MerkleProofLib, MockTiramisu);
   deployer
     .deploy(MockToken, 5000, "Mock ERC20 Token", "MOCK")
     .then(deployed => deployer.deploy(MockTiramisu, deployed.address));
